b3234e30717d3fdbaf9cd21aef5b7283af121272
Ryan <rkwarner@dplabs.ca>
2023-12-06 00:02:39 -0800

    includes scatter plot

--- query_json.py
+++ query_json.py
@@ -2,16 +2,23 @@
 
 import json
 import os
+import pickle
+import argparse 
 path = "json/"
 files = [f for f in os.listdir(path)]
-# print("files \n",files)
 
+def boolean_string(s):
+    if s not in {'False', 'True'}:
+        raise ValueError('Not a valid boolean string')
+    return s == 'True'
 
-import argparse 
 parser = argparse.ArgumentParser(description='Process some integers.')
 
 parser.add_argument('--tag', type=str, help='tags to search for')
+parser.add_argument('--newquery', type=boolean_string, default=False, help='run new query')
+parser.add_argument('--load', type=boolean_string, default=False, help='load from save file')
 args = parser.parse_args()
+print(args)
 args.tag = "<" + args.tag + ">"
 
 def checkKey(d=None, key=None):
@@ -175,7 +182,6 @@ def queryOwners(tagged_posts=None, vote_counts=None):
 
 
 def saveDict(dict):
-    import pickle
     with open('data/' + args.tag + '.pickle', 'wb') as handle:
         pickle.dump(dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
@@ -183,40 +189,122 @@ def saveDict(dict):
         d_dict = pickle.load(handle)
         print(dict == d_dict)
 
-questions = getQuestions()
-answers = getAnswers()
 
-# tagged_Questions = queryQuestions(questions_list=questions, tag="<machine-learning>")
-# tagged_Questions = queryQuestions(questions_list=questions, tag="<python>")
-# tagged_Questions = queryQuestions(questions_list=questions, tag=args.tag)
+def sum_userVotes():
+    d_dict = None
+    with open('data/' + args.tag + '.pickle', 'rb') as handle:
+        d_dict = pickle.load(handle)
+        
+    
+    
+    sorted_keys = sorted(d_dict.keys())
+    
+    sorted_keys = [eval(i) for i in sorted_keys]
+    sorted_keys.sort()
+    
+    accepted_vote_list = []
+    spam_vote_list = []
+    up_vote_list = []
+    down_vote_list = []
 
-# print("len(tagged questions: ) --> ", len(tagged_Questions))
-# tagged_Answers = queryAnswers(answers_list=answers, tagged_PostList=tagged_Questions)
-# print("len(tagged_Answers) --> ", len(tagged_Answers))
+    
+    for idx, key in enumerate(sorted_keys):
+        
+        d = d_dict[str(key)]
+        tmp = sorted(d_dict[str(key)].keys())
+        d = d[tmp[0]]
 
-# tagged_Votes = queryVotes(tagged_list=tagged_Answers)
-# vote_Counts = voteAnalysis(tagged_votes=tagged_Votes, tagged_posts=tagged_Answers)
+        up_votes = 0
+        down_votes = 0
+        accepted_votes = 0
+        spam_votes = 0
+        
+        for vote_types in d.keys():
 
-# owner_post_VoteCounts = queryOwners(tagged_posts=tagged_Answers, vote_counts=vote_Counts)
-# saveDict(owner_post_VoteCounts)
+            if vote_types == '1':
+                accepted_votes += d['1']
+                
+            elif vote_types == '2':
+                up_votes += d['2']
 
-# print(owner_post_VoteCounts)
+            elif vote_types == '3':
+                down_votes += d['3']
+            
+            elif vote_types == '12':
+                spam_votes += d['12']
 
+                
+        up_vote_list.append(up_votes)
+        down_vote_list.append(down_votes)
+        accepted_vote_list.append(accepted_votes)
+        spam_vote_list.append(spam_votes)
+            
+    return [up_vote_list, down_vote_list, accepted_vote_list, spam_vote_list]
+    
+def makeHistogram(vote_list=None):
+    raise NotImplementedError
+
+
+
+def makeScatterPlot(vote_list=None):
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    # Fixing random state for reproducibility
+    up_votes =  np.array(vote_list[0]).astype(np.float32)
+    down_votes = np.array(vote_list[1]).astype(np.float32)
+    
+    accepted_votes = np.array(vote_list[2]).astype(np.float32)
+    spam_votes =  np.array(vote_list[3]).astype(np.float32)
+    
+    x = up_votes - down_votes
+    y = accepted_votes - spam_votes
+    y = accepted_votes
+    
+    # print(accepted_votes[0:100])
+    # print(spam_votes[0:100])
+    
+    # print(np.max(accepted_votes))
+    # print(np.min(down_votes))
+    
+    print(np.max(x))
+    print(np.min(x))
+    
+    
+    N = len(x)
+    colors = np.random.rand(N)
+    area = ((30 * np.random.rand(N))**2).astype(np.float32)  # 0 to 15 point radii
+
+    plt.scatter(x, y, s=area, c=colors, alpha=0.5)
+    # plt.scatter(x, y, c=colors, alpha=0.5)
+    plt.scatter(x, y)
+    plt.show() 
+    plt.savefig("data/imgs/" + args.tag + '.png')   
 
 def main():
-    print("Hello World!")
-    tagged_Questions = queryQuestions(questions_list=questions, tag=args.tag)
+    # print("Hello World!")
 
-    print("len(tagged questions: ) --> ", len(tagged_Questions))
-    tagged_Answers = queryAnswers(answers_list=answers, tagged_PostList=tagged_Questions)
-    print("len(tagged_Answers) --> ", len(tagged_Answers))
+    if args.newquery and not args.load:
+        questions = getQuestions()
+        answers = getAnswers()
+        tagged_Questions = queryQuestions(questions_list=questions, tag=args.tag)
 
-    tagged_Votes = queryVotes(tagged_list=tagged_Answers)
-    vote_Counts = voteAnalysis(tagged_votes=tagged_Votes, tagged_posts=tagged_Answers)
+        print("len(tagged questions: ) --> ", len(tagged_Questions))
+        tagged_Answers = queryAnswers(answers_list=answers, tagged_PostList=tagged_Questions)
+        print("len(tagged_Answers) --> ", len(tagged_Answers))
 
-    owner_post_VoteCounts = queryOwners(tagged_posts=tagged_Answers, vote_counts=vote_Counts)
-    saveDict(owner_post_VoteCounts)
+        tagged_Votes = queryVotes(tagged_list=tagged_Answers)
+        vote_Counts = voteAnalysis(tagged_votes=tagged_Votes, tagged_posts=tagged_Answers)
 
+        owner_post_VoteCounts = queryOwners(tagged_posts=tagged_Answers, vote_counts=vote_Counts)
+        saveDict(owner_post_VoteCounts)
+        vote_list = sum_userVotes()
+        makeScatterPlot(vote_list=vote_list)
+        
+    elif args.load:
+        vote_list = sum_userVotes()
+        makeScatterPlot(vote_list=vote_list)
+    
 if __name__ == "__main__":
     main()
 
